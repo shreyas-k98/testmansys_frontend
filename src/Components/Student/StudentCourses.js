a38421e9795b89d37f7912cd7f1071c8ab969de9
CallMeShreyas <shreyaskulkarni@tuta.io>
2022-10-31 22:00:54 +0530

    bug fix

--- src/Components/Student/StudentCourses.js
+++ src/Components/Student/StudentCourses.js
@@ -4,64 +4,71 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+
 const StudentCourses = () => {
+  const [courses, setCourses] = useState([]);
   axios.defaults.headers.common["csrftoken"] =
-    localStorage.getItem("csrf_token"); // for all requests
+    localStorage.getItem("csrf_token");
 
-  const [courses, setC] = useState([]);
-  console.log("---", courses);
-
-  useEffect(() => {
-    fetchCourses();
-  }, []);
-
-  const fetchCourses = async () => {
     let baseurl = "/course/";
-    await axios
-      .get(baseurl)
-      .then((response) => {
-        // let courses = []
-        setC(response?.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+    axios.get(baseurl).then((responce) => {
+      let courses = responce.data
+      setCourses(courses)
+  
+    });
 
-  const enroll = (course_name) => {
+    // useEffect = () => {
+    //   enroll(course.course_name)
+    // }
 
-  }
-
+  // const enroll = (course_name) => {
+  //   let baseurl = "/enroll/";
+  //   axios
+  //     .post(baseurl, {
+  //       "course_enrolled": course_name,
+  //     })
+  //     .then(function (response) {
+  //       console.log(response.data);
+  //       alert("Enrolled !!!");
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // };
 
   return (
     <div>
       <StudentHeader />
-      <div className="container">
-        {courses?.length === 0 ? (
-          <div className="text-center">
-            <h3>No Courses To Display</h3>
-          </div>
-        ) : (
-          <div className="container">
-            <div className="row">
-              {courses?.map((course) => {
-                return (
-                  <div className="col-4">
-                    <div className="card" style={{ width: "18rem" }}>
-                      <div className="card-body">
-                        <h5 className="card-title">{course["course_name"]}</h5>
-                        <p className="card-text">{course["creator_name"]}</p>
-                        <Link className="btn btn-success" onClick={() => {enroll(course['course_name'])}}>
-                          Enroll
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
+      <div>
+        {courses.map((course) => {
+          return (
+            <div className="container">
+              <div className="card">
+                <div clasNames="card-body">
+                  <h5 clasNames="card-title">{course.course_name}</h5>
+                  <p className="card-text">{course.creator_name}
+                  </p>
+                  <button onClick = {()=>{
+                      let baseurl = "/course/enroll/";
+                        axios
+                          .post(baseurl, {
+                            "course_enrolled": course.course_name,
+                          })
+                          .then(function (response) {
+                            console.log(response.data);
+                            alert("Enrolled !!!");
+                          })
+                          .catch(function (error) {
+                            console.log(error);
+                          });
+                  }}className="btn btn-success">
+                    Enroll
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        )}
+          );
+        })}
       </div>
       <Footer />
     </div>
